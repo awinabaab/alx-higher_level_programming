24cfa819a4d58bf6071f7acf02bb6c74d518c4b0
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-09 08:32:22 +0000

    Conform to pycodestyle

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = f"{str[39:66]} with {str[:6]}"
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str = f"{str[39:66]} {str[110:114]} {str[:6]}"
 print(str)
