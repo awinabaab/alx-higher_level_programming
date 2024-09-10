ec334f5b78f8ebda8e026e8609525005969e54da
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-10 03:07:25 -0700

    Add 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        print(f"{(ord(char) - 32 if 96 < ord(char) < 123 else ord(char)):c}",
+                    end='')
+    print('\n', end='')
+
