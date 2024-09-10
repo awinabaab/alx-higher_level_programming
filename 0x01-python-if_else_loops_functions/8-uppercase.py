42eae4a941db7ab3cb2fa6023635a15037e9cc6d
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-10 10:39:25 +0000

    Conform to pycodestyle

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,5 @@
 def uppercase(str):
     for char in str:
         print(f"{(ord(char) - 32 if 96 < ord(char) < 123 else ord(char)):c}",
-                    end='')
+              end='')
     print('\n', end='')
-
