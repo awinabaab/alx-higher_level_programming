14913ff34a91299e98623788a09a80828be726a1
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-10 13:00:15 +0000

    Change f-string format to .format function call

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('a') - 1, -1):
-    print((f"{char:c}") if (char % 2 == 0) else (f"{(char - 32):c}"), end='')
+    print("{:c}".format(char) if (char % 2 == 0) else "{:c}".format(char - 32),
+          end='')
