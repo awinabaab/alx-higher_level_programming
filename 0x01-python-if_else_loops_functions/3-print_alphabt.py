a7a9dfb05903c2ec88f1234b7dc4df9a0a34402a
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-10 12:28:32 +0000

    Change f-string format to .format function call

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for alphabet in range(ord('a'), ord('z') + 1):
     if chr(alphabet) == 'q' or chr(alphabet) == 'e':
         continue
     else:
-        print(f"{alphabet:c}", end='')
+        print("{:c}".format(alphabet), end='')
