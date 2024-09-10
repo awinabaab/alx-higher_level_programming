246abc562d211307093673ce72c593d91ade9c15
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-10 20:54:12 +0000

    Change f-string format to .format function call

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char in str:
-        print(f"{(ord(char) - 32 if 96 < ord(char) < 123 else ord(char)):c}",
-              end='')
+    for index in range(0, len(str)):
+        char = str[index]
+        print("{}".format(chr(ord(char) - 32)) if 96 < ord(str[index]) < 123
+              else "{}".format(char), end='')
     print('\n', end='')
