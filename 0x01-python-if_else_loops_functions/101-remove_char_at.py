e07c6b70ba9cce7de835b15e57152a799765807c
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-12 06:03:52 +0000

    Refactor function

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     string = str[0:n] + str[n+1:]
-    if n < 0:
-        return (str)
-    else:
-        return (string)
+    return (str if n < 0 else string)
