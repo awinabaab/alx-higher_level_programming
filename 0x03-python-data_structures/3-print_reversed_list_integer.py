a7e6878659e7b375b65ac5edbd452ef5de5aeb3b
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-14 14:13:16 +0000

    Return reversed list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    rev_list = my_list[::-1]
-    for item in sorted(my_list, reverse=True):
+    rev_list = sorted(my_list, reverse=True)
+    for item in rev_list:
         print("{:d}".format(item))
+
+    return rev_list
