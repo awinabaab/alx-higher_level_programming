87652b7fa36d0eb624986b926f406f65eb0d93bc
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-14 07:01:35 +0000

    Handle case for funtion call with empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0])
+    sentence_len = len(sentence)
+    first_char = sentence[0] if sentence_len > 0 else None
+    return (sentence_len, first_char)
