60258a7dbcadd6b8bc9e0366d08d7d1ea93aed2c
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-14 08:20:51 +0000

    Replace for loop with simple assignment

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,5 @@ def new_in_list(my_list, idx, element):
     list_copy = my_list[:]
     if idx < 0 or idx >= len(my_list):
         return list_copy
-    for index in range(len(list_copy)):
-        if index == idx:
-            list_copy[index] = element
-            return list_copy
+    list_copy[idx] = element
+    return list_copy
