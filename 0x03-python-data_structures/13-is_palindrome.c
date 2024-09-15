bde8754b0d5b7863abaf0c046fa0d02d77b13b94
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-15 09:41:04 +0000

    Implement an efficient version

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,81 +9,35 @@
 
 int is_palindrome(listint_t **head)
 {
+	listint_t *fast_ptr = *head;
+	listint_t *slow_ptr = *head;
 	listint_t *head_node = *head;
-	listint_t *duplicate_head = NULL;
+	listint_t *mid_ptr = NULL;
 	listint_t *reverse_head = NULL;
-	listint_t *reverse_node = NULL;
+	listint_t *reverse_head_cpy = NULL;
 
-	if (!*head)
-		return (1);
-	duplicate_head = duplicate_list(&head_node);
-	reverse_head = reverse_list(&duplicate_head);
-
-	reverse_node = reverse_head;
-
-	while (reverse_node->next && head_node->next)
+	while (slow_ptr && fast_ptr && fast_ptr->next)
 	{
-		if (reverse_node->n != head_node->n)
+		if (fast_ptr == NULL)
+			break;
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+	}
+
+	mid_ptr = slow_ptr;
+	reverse_head = reverse_list(&mid_ptr);
+	reverse_head_cpy = reverse_head;
+
+	while (reverse_head->next && head_node->next)
+	{
+		if (reverse_head->n != head_node->n)
 		{
-			free_listint(reverse_head);
+			reverse_head = reverse_list(&reverse_head_cpy);
 			return (0);
 		}
-		reverse_node = reverse_node->next;
+		reverse_head = reverse_head->next;
 		head_node = head_node->next;
 	}
-
-	free_listint(reverse_head);
+	reverse_head = reverse_list(&reverse_head_cpy);
 	return (1);
-}
-
-/**
- * duplicate_list - Duplicates a singly linked list
- * @head: Pointer to the head node of the singly linked list
- *
- * Return: Pointer to head of duplicate @head
- */
-
-listint_t *duplicate_list(listint_t **head)
-{
-	listint_t *head_node = *head;
-	listint_t *duplicate = NULL;
-
-	if (!*head)
-		return (NULL);
-
-	while (head_node != NULL)
-	{
-		add_nodeint_end(&duplicate, head_node->n);
-		head_node = head_node->next;
-	}
-
-	return (duplicate);
-}
-
-/**
- * reverse_list - Reverses a singly linked list
- * @head: Pointer to the head node of the singly linked list
- *
- * Return: Pointer to head node of reversed list else NULL
- */
-
-listint_t *reverse_list(listint_t **head)
-{
-	listint_t *head_node = *head;
-	listint_t *current_node = NULL;
-	listint_t *next_node = NULL;
-
-	if (!*head)
-		return (NULL);
-
-	while (head_node != NULL)
-	{
-		next_node = head_node->next;
-		head_node->next = current_node;
-		current_node = head_node;
-		head_node = next_node;
-	}
-	head_node = current_node;
-
-	return (head_node);
 }
