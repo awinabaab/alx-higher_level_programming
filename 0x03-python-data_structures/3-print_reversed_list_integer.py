4653f30954db1a0bad15569cbfe9a0fa28c5f8fc
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-15 05:59:45 +0000

    Handle NULL case for function argument

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    rev_list = sorted(my_list, reverse=True)
-    for item in rev_list:
-        print("{:d}".format(item))
-
-    return rev_list
+    if my_list:
+        for idx in range(len(my_list) -1 , -1 , -1):
+            print("{:d}".format(my_list[idx]))
