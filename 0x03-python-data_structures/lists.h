a6f9b242e29fc39933ad35fa8b4785affc42a2e2
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-16 06:43:55 +0000

    Include Python.h header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <Python.h>
 
 /**
  * struct listint_s - singly linked list
@@ -24,5 +25,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 listint_t *reverse_list(listint_t **head);
+void print_python_list_info(PyObject *p);
 
 #endif /* LISTS_H */
