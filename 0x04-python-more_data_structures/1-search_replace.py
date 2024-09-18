a2a5bd806473c0b63f5de848ce2693fb57380095
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-18 14:12:53 +0000

    Use map function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for idx in range(0, len(new_list)):
-        if new_list[idx] == search:
-            new_list[idx] = replace
-    return (new_list)
+    new_list = list(map(lambda item:
+                    replace if item == search else item, my_list)
+                    )
+    return new_list
