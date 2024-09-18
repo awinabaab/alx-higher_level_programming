15d642524e38eb5dd95a5319e87ef9b038c0b339
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-18 14:14:27 +0000

    Use sum function

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    result = 0
-    uniq_list = set(my_list)
-    for num in uniq_list:
-        result += num
-    return result
+    return sum(set(my_list))
