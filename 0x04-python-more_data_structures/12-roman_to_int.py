c3dc84f7f13b3dff9b1b02635c09ff9695fc8ec3
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-18 13:19:18 +0000

    Include conditional check for previous and current values

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    result = 0
-    roman_numerals = {
-                        "I": 1,
-                        "V": 5,
-                        "X": 10,
-                        "L": 50,
-                        "C": 100,
-                        "D": 500,
-                        "M": 1000
-                     }
-    for char in roman_string:
-        if char in roman_numerals:
-            result += roman_numerals[char]
-    return result
+    if type(roman_string) == str:
+        result = 0
+        curr_value = 0
+        prev_value = 0
+        roman_numerals = {
+                            "I": 1,
+                            "V": 5,
+                            "X": 10,
+                            "L": 50,
+                            "C": 100,
+                            "D": 500,
+                            "M": 1000
+                         }
+        for char in reversed(roman_string):
+            curr_value = roman_numerals[char]
+            if curr_value < prev_value:
+                result -= curr_value
+            else:
+                result += curr_value
+            prev_value = curr_value
+        return result
+    else:
+        return 0
