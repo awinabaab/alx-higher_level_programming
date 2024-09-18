a400efeaeefebb9c39524ce0f69e68688ef637d4
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-18 12:21:12 +0000

    Remove conditional check

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        result = 0
-        uniq_list = set(my_list)
-        for num in uniq_list:
-            result += num
-        return result
+    result = 0
+    uniq_list = set(my_list)
+    for num in uniq_list:
+        result += num
+    return result
