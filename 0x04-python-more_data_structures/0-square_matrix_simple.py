09b04db89226b719e481582a161535bf1270fa95
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-18 14:09:17 +0000

    Use map function

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix:
-        new_matrix = []
-        for idx in range(0, len(matrix)):
-            square = list(map(lambda num: num ** 2, matrix[idx]))
-            new_matrix.append(square)
-        return (new_matrix)
+        new_matrix = list(map(lambda row:
+                              list(map(lambda num: num ** 2, row)),
+                              matrix)
+                          )
+        return new_matrix
