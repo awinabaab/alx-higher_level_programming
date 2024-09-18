34f5da7865a8f77c67e23410b0db547acb963871
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-18 16:30:14 +0000

    Use map function

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = a_dictionary.copy()
-    for key, value in new_dictionary.items():
-        new_dictionary[key] = value * 2
-    return new_dictionary
+    return dict(map(lambda key_val: (key_val[0], key_val[1] * 2),
+                    a_dictionary.items()))
