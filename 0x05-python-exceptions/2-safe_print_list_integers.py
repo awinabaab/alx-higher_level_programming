f333a8f5ac34653bae8e5b25348308c65f7cd41c
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 17:54:19 +0000

    Fix return value

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    if not my_list or x == 0:
+    if not my_list or x < 1:
         return 0
+    printed = 0
     try:
         for y in range(0, x):
             try:
                 print("{:d}".format(my_list[y]), end="")
-            except ValueError:
+                printed += 1
+            except (TypeError, ValueError):
                 continue
         print()
-    except (TypeError, IndexError):
+    except (IndexError):
         print()
-        return (y - 1)
-    else:
-        return (y + 1)
+    finally:
+        return (printed)
