9c35f604d764604b6f6cc38b6f7572a13b14aa71
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 17:04:38 +0000

    Use finally statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     if not my_list or x == 0:
         return 0
     try:
         for idx in range(0, x):
-            try:
-                print("{}".format(my_list[idx]), end="")
-            except (IndexError, TypeError, KeyError):
-                idx -= 2
+            print("{}".format(my_list[idx]), end="")
         print()
+    except (IndexError, TypeError, KeyError):
+        print()
+        idx -= 1
     finally:
         return (idx + 1)
