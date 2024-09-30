fc7f37725012caee305c7f7a5d8679a7d275e5c6
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 12:19:52 +0000

    Add 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for y in range(0, x):
+            print("{}".format(my_list[y]), end="")
+        print()
+    except IndexError:
+        print()
+        return y
+    else:
+        return (y + 1)
