5930700a0c6d728a55985177aaf64bdfe4e5146a
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 12:27:36 +0000

    Check for empty list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    if not my_list:
+        return 0
     try:
         for y in range(0, x):
             print("{}".format(my_list[y]), end="")
