8002cf035da436c6dff69da48a7d0f82d366d1e4
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 12:55:47 +0000

    Handle KeyError exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         for y in range(0, x):
             print("{}".format(my_list[y]), end="")
         print()
-    except (IndexError, TypeError):
+    except (IndexError, TypeError, KeyError):
         print()
         return y
     else:
