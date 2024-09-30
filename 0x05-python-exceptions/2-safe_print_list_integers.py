b2add72413587a4bfbbc9bd888e4225ae7703498
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 22:04:34 +0000

    Allow IndexError to occur

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
-    try:
-        for y in range(0, x):
-            try:
-                print("{:d}".format(my_list[y]), end="")
-                printed += 1
-            except (TypeError, ValueError):
-                continue
-    except IndexError:
-        pass
+    for y in range(0, x):
+        try:
+            print("{:d}".format(my_list[y]), end="")
+            printed += 1
+        except (TypeError, ValueError):
+            continue
     print()
     return printed
