637d9661b225fdb9e62add44cb913689f2860ceb
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 16:31:31 +0000

    Fix: return the actual number of items printed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,11 @@ def safe_print_list(my_list=[], x=0):
     if not my_list or x == 0:
         return 0
     try:
-        for y in range(0, x):
-            print("{}".format(my_list[y]), end="")
+        for idx in range(0, x):
+            try:
+                print("{}".format(my_list[idx]), end="")
+            except (IndexError, TypeError, KeyError):
+                idx -= 2
         print()
-    except (IndexError, TypeError, KeyError):
-        print()
-        return y
-    else:
-        return (y + 1)
+    finally:
+        return (idx + 1)
