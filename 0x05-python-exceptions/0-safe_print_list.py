97f5e82aa7a3cb84dac62d01ddc6ab3f6ed10f46
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 17:17:51 +0000

    Return zero if x < 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if not my_list or x == 0:
+    if not my_list or x < 1:
         return 0
     try:
         for idx in range(0, x):
             print("{}".format(my_list[idx]), end="")
         print()
-    except (IndexError, TypeError, KeyError):
-        print()
-        idx -= 1
-    finally:
         return (idx + 1)
+    except (IndexError, TypeError):
+        print()
+        return (idx)
