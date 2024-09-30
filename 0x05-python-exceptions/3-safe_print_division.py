95434662a4a0b68a5cd131c8ebe7b83df1e9f530
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 15:26:39 +0000

    Add 3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    division = 0
+    try:
+        division = a / b
+        return division
+    except ZeroDivisionError:
+        division = None
+    finally:
+        print("Inside result: {}".format(division))
