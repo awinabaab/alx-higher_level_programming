1f0afce6405bb9467676d5222756d0b79a944a43
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 22:00:41 +0000

    Add required try statement

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = [0 for num in range(list_length)]
-    for idx in range(0, list_length):
-        try:
-            new_list[idx] = my_list_1[idx] / my_list_2[idx]
-        except ZeroDivisionError:
-            print("division by 0")
-        except IndexError:
-            print("out of range")
-        except TypeError:
-            print("wrong type")
-    return new_list
+    try:
+        for idx in range(0, list_length):
+            try:
+                new_list[idx] = my_list_1[idx] / my_list_2[idx]
+            except ZeroDivisionError:
+                print("division by 0")
+            except IndexError:
+                print("out of range")
+            except TypeError:
+                print("wrong type")
+    finally:
+        new_list
