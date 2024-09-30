20dd100961e08933ca5d2ae7b1caad6c9e196758
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 21:55:32 +0000

    Remove redundant try statement

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = [0 for num in range(list_length)]
-    try:
-        for idx in range(0, list_length):
-            try:
-                new_list[idx] = my_list_1[idx] / my_list_2[idx]
-            except ZeroDivisionError:
-                print("division by 0")
-            except IndexError:
-                print("out of range")
-            except TypeError:
-                print("wrong type")
-    finally:
-        return new_list
+    for idx in range(0, list_length):
+        try:
+            new_list[idx] = my_list_1[idx] / my_list_2[idx]
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        except TypeError:
+            print("wrong type")
+    return new_list
