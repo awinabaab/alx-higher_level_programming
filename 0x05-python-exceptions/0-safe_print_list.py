251086218e8d9e99bb4d19dcffc2875a04ab9c9f
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-09-30 18:02:46 +0000

    Fix return value

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     if not my_list or x < 1:
         return 0
+    printed = 0
     try:
         for idx in range(0, x):
             print("{}".format(my_list[idx]), end="")
+            printed += 1
         print()
-        return (idx + 1)
     except (IndexError, TypeError):
         print()
-        return (idx)
+    finally:
+        return printed
