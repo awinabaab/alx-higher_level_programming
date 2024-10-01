02615188e69e4fa56ff85e497ee1603795cb8cd3
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-10-01 12:23:48 +0000

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class that defines a square with a size.
+"""
+
+
+class Square:
+    """A class that defines a square with a size.
+    """
+    def __init__(self, size):
+        """Initialize a private instance field size
+
+        Args:
+            size: the size of the instance(private)
+        """
+        self.__size = size
