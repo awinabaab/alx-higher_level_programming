2c33578a5103ff44c5ae0b45b256f15735c937ea
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-10-09 07:07:44 +0000

    Fix: correct output message

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,11 +11,11 @@ def say_my_name(first_name, last_name=""):
         last_name (str): last_name
 
     Raises:
-        TypeError: if either of the argumens are not strings
+        TypeError: if either of the arguments are not strings
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("{} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
