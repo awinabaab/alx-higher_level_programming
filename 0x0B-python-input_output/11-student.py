a38e014558b1c1fef946231c7f943b18679c54e3
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-10-15 13:42:11 +0000

    Use dictionary comprehension

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -30,4 +30,4 @@ class Student:
     def reload_from_json(self, json):
         """Replaces all the attributes of the Student instance with @json
         """
-        self.__dict__ = json
+        self.__dict__ = {k:v for (k, v) in json.items()}
