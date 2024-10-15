545727401b30ac3ec46fbeea469bd9b1ca1c99d0
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-10-15 12:53:32 +0000

    Add 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""8-class_to_json.py module
+"""
+import json
+
+
+def class_to_json(obj):
+    """Returns the dictionary decription with simple data structure
+    for JSON serialization of an object
+    """
+    return (obj.__dict__)
