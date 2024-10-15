2f4482f1b7b83bd55a6fcf7da75e8d994d278fd4
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-10-15 12:43:15 +0000

    Add 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""6-load_from_json_file.py module
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return (json.load(f))
