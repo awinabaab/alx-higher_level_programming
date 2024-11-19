c29e54853b70c81ad01e9a6178e9a0d32d770bb1
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-11-19 11:59:39 +0000

    Add 4-first_table.sql script

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Creates a table called first_table in the current database in your MySQL server
+-- The database name will be passed as an argument of the mysql command
+
+CREATE TABLE first_table (
+	id INT,
+	name VARCHAR(256)
+);
