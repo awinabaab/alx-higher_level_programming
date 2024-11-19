c208bdcf9ad5cdd89692e17c8f2bbace9a75d948
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-11-19 12:02:31 +0000

    Add 14-average.sql script

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- The results column name should be average
+-- The database name will be passed as an argument of the mysql command
+
+SELECT AVG(score) AS average FROM second_table;
