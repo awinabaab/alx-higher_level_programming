17c2407373aff15ce521df61e7339b9896e09110
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-11-19 15:14:50 +0000

    Use the CONVERT TO statement

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,7 +7,7 @@
 USE hbtn_0c_0;
 
 ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
+CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
@@ -16,5 +16,5 @@ COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
+CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
