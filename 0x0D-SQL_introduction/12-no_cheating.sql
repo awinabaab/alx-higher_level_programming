199679cea9022da98921b6ff4ebf35e6cbe3fc01
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-11-19 12:01:51 +0000

    Add 12-no_cheating.sql script

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Updates the score of Bob in the table second_table of the database hbtn_0c_0 in your MySQL server
+-- Use only the field name to update the record
+-- The database name will be passed as an argument of the mysql command
+
+UPDATE second_table SET score = 10 WHERE name = "Bob";
