12cad02fb769a012606029a7a07f43b2c91ff934
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-11-20 12:44:08 +0000

    Add 8-cities_of_california_subquery.sql script

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- Lists all the cities of California that can be found in the database hbtn_0d_usa
+-- The states table contains only one record where name = California
+-- Results must be sorted in the ascending order by cities.id
+-- The database name will be passed as an argument of the mysql command
+
+SELECT * FROM cities WHERE state_id = 1;
