33ea365754f30cdf52a1cce896fbfd437d9f47d7
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-12-18 23:09:09 +0000

    Use parseInt function

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const number = Math.floor(Number(process.argv[2]));
+const number = parseInt(Number(process.argv[2]));
 
 if (number) console.log(`My number: ${number}`);
 else console.log('Not a number');
