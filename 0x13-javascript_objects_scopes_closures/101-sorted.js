e9d090f5ef9a7a3b0a2b53b5e9b2df1d163bf327
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-12-20 19:57:27 +0000

    Add 101-sorted.js 101-data.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const newDict = {};
+
+for (const key in dict) {
+  const value = dict[key];
+
+  if (!newDict[value]) newDict[value] = [];
+  newDict[value].push(key);
+}
+
+console.log(newDict);
