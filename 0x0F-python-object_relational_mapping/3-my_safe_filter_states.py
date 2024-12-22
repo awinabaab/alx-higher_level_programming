df155beb2d09d0412d70be500c73cf98100bcdf8
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-12-22 19:08:05 +0000

    Add 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Displays all values in the states table where name matches query"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db_host = 'localhost'
+    db_user = argv[1]
+    db_passwd = argv[2]
+    db_name = argv[3]
+    db_queryfilter = argv[4]
+    db_port = 3306
+
+    db_connection = MySQLdb.connect(host=db_host,
+                                    user=db_user,
+                                    passwd=db_passwd,
+                                    db=db_name,
+                                    port=db_port
+                                    )
+
+    query = """
+                SELECT *
+                FROM states
+                WHERE states.name = %s
+                ORDER BY states.id ASC
+            """
+
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(query, (db_queryfilter,))
+
+    for state in db_cursor:
+        print(state)
+
+    db_cursor.close()
+    db_connection.close()
