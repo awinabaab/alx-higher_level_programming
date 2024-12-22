6a419ddebd26dfa08ad3a97fc7dcbdc2ada731cd
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-12-22 21:56:49 +0000

    Remove redundant print statement

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -28,7 +28,6 @@ if __name__ == "__main__":
     session = Session()
 
     cities = session.query(City).order_by(City.id).all()
-    print(session.query(City))
     for city in cities:
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
 
