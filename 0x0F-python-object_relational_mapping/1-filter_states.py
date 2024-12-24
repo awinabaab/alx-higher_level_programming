879a187c83d58e1ccd053cb62f4edb7baa682579
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-12-24 09:41:18 +0000

    Correct typo in query string

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
                 SELECT *
                 FROM states
                 WHERE states.name
-                LIKE BINARYo 'N%'
+                LIKE BINARY 'N%'
                 ORDER BY states.id ASC;
             """
 
